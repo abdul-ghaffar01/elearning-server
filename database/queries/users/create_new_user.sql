9c2a96457ce746b49783911b3faf6c0533c6271d
Abdul Ghaffar <agscontact777@gmail.com>
2025-12-04 21:57:05 +0500

    returning profile_setupped attribute on login

--- database/queries/users/create_new_user.sql
+++ database/queries/users/create_new_user.sql
@@ -1,3 +1,3 @@
 INSERT INTO users (fullname, email, profile)
 VALUES ($1, $2, $3)
-RETURNING id, fullname, email, profile, joined_at;
+RETURNING id, fullname, email, profile, joined_at, profile_setupped;
--- database/queries/users/find_user_by_email.sql
+++ database/queries/users/find_user_by_email.sql
@@ -1,2 +1,2 @@
-SELECT id, fullname, email, profile, joined_at
+SELECT id, fullname, email, profile, joined_at, profile_setupped
 FROM users WHERE email = $1
